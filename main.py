f0d60bcaab4ba3cdae3da3c466f72a2503af6a2f
yungs <yungs9003@naver.com>
2023-03-30 18:59:29 +0900

    write a line of comment

---
+++ main.py
@@ -0,0 +1,10 @@
+# Main function
+def main():
+# Example list
+int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_int_list = even_list(int_list)
+output = sum_of_squares_of_even(even_int_list)
+print(output)
+# Boilerplate code
+if __name__ == "__main__":
+main()
